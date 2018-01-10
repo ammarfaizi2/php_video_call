abc5fd738bd6571fb6457e2b46eba908ed665f9d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-10 19:05:23 +0700

    Update host

--- ws/bin/server.php
+++ ws/bin/server.php
@@ -7,15 +7,15 @@
  * @date 23-Dec-2016
  */
 
-require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Amir\Comm;
 
 //set an array of origins allowed to connect to this server
-$allowed_origins = ['localhost', '127.0.0.1'];
+$allowed_origins = ['localhost', '127.0.0.1', 'ammarfaizi.com'];
 
 // Run the server application through the WebSocket protocol on port 8080
-$app = new Ratchet\App('localhost', 8080, '0.0.0.0');//App(hostname, port, 'whoCanConnectIP', '')
+$app = new Ratchet\App('ammarfaizi.com', 8080, '0.0.0.0');//App(hostname, port, 'whoCanConnectIP', '')
 
 //create socket routes
 //route(uri, classInstance, arrOfAllowedOrigins)
